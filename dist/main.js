d47af11a848b4433d70e3428934628cc78e1c4d7
Talon Bragg <pvpgamecrash@gmail.com>
2016-12-21 19:05:00 -0800

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -1,27 +1,8 @@
-var copyThat = document.getElementById('copy');
-selectText = function (element) {
-        var range, selection;    
-        if (document.body.createTextRange) {
-          range = document.body.createTextRange();
-          range.moveToElementText(element);
-          range.select();
-        } else if (window.getSelection) {
-          selection = window.getSelection();        
-          range = document.createRange();
-          range.selectNodeContents(element);
-          selection.removeAllRanges();
-          selection.addRange(range);
-        }
-      }
-      copyColor = function(e) {
-        var copyTextDiv = e.target;
-        selectText(copyTextDiv);
-        try {
-          var copied = document.execCommand('copy');
-          var msg = copied ? 'successful.' : 'unsuccessful.';
-          alert('Copy' + msg);
-        } catch (err) {
-          console.log('Unable to copy on this browser.');
-        }            
-      };
-      copyThat.addEventListener('click', copyColor);
+/*
+* CopyThat 0.1.0
+* Copyright 2016, Talon Bragg
+* https://github.com/talonbragg/CopyThat/
+* Listed under the Apache 2.0.0 license.
+* https://copythat.ml
+*/
+var copyThat=document.getElementById("copy");selectText=function(a){var b,c;document.body.createTextRange?(b=document.body.createTextRange(),b.moveToElementText(a),b.select()):window.getSelection&&(c=window.getSelection(),b=document.createRange(),b.selectNodeContents(a),c.removeAllRanges(),c.addRange(b))},copyColor=function(a){var b=a.target;selectText(b);try{var c=document.execCommand("copy"),d=c?"successful.":"unsuccessful.";alert("Copy"+d)}catch(a){console.log("Unable to copy on this browser.")}},copyThat.addEventListener("click",copyColor);
