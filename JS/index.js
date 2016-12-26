6956930d64c0dde4f863d668f3e4b32eec2ad634
Talon Bragg <pvpgamecrash@gmail.com>
2016-12-26 09:44:13 -0800

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -8,10 +8,19 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("content").style.display = "block";
 }
+var closeIcon = document.getElementById('navBtn');
 function openNav() {
             document.getElementById("mySidenav").style.width = "250px";
             document.getElementById("main").style.marginLeft = "250px";
             document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+            if(document.getElementById('mySidenav').style.marginLeft === "250px") {
+                function close(e) {
+                    document.getElementById("mySidenav").style.width = "0";
+                    document.getElementById("main").style.marginLeft = "0";
+                    document.body.style.backgroundColor = "white";
+                }
+                    closeIcon.addEventListener('click', close);
+               }
         }
         function closeNav() {
             document.getElementById("mySidenav").style.width = "0";
