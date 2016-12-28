6f2ca203199c8d7fb5b30fd342bbc34c9870a5fd
Talon Bragg <pvpgamecrash@gmail.com>
2016-12-28 14:25:33 -0800

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,6 @@
 var myVar,
     open = false;
+var time;
 
 function myFunction() {
     myVar = setTimeout(showPage, 3000);
@@ -14,8 +15,11 @@ function openNav() {
     if(open) {
         open = false;
                     document.getElementById("mySidenav").style.width = "0";
-                    $('#navLink').slideIn('slow');
                     document.body.style.backgroundColor = "white";
+                    time = setTimeou('showLink', 1000);
+                    function showLink() {
+                        $('#navLink').slideIn('slow');
+                    }
             }
     else {
         open = true;
@@ -26,7 +30,7 @@ function openNav() {
         function closeNav() {
             document.getElementById("mySidenav").style.width = "0";
             document.getElementById("main").style.marginLeft = "0";
-            document.body.style.backgroundColor = "white";
+            document.body.style.backgroundColor = "white";
         }
 window.onblur = function () { document.title = 'Please Come Back!'; }
 window.onfocus = function () { document.title = 'Copy That'; }
