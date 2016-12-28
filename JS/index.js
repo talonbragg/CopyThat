83a2677a5cec3e85690619a28380b256b8fde617
Talon Bragg <pvpgamecrash@gmail.com>
2016-12-28 12:27:00 -0800

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -14,13 +14,11 @@ function openNav() {
     if(open) {
         open = false;
                     document.getElementById("mySidenav").style.width = "0";
-                    document.getElementById("main").style.marginLeft = "0";
                     document.body.style.backgroundColor = "white";
             }
     else {
         open = true;
         document.getElementById("mySidenav").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
         document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
     }
             }
